7c040994ce7835754ad2072f309874ad5d19dbe8
william.li <william.li@ll.mit.edu>
2023-12-19 15:07:39 -0500

    Updating CLI with short options
    Former-commit-id: 97cb0cda54e2cb944f87afa07e86b7f8dcf83836

--- test/cli_test.py
+++ test/cli_test.py
@@ -5,13 +5,5 @@ from src.main import app
 runner = CliRunner()
 
 
-# def test_setup():
-#     result1 = runner.invoke(app, ["setup-head", "--partition manycore"])
-#     result2 = runner.invoke(app, ["setup-cpu", "--partition manycore"])
-#     result3 = runner.invoke(app, ["setup-gpu", "--partition gaia"])
-
-#     # assert "success" in result.output
-
-
 def test_run():
     result = runner.invoke(app)
